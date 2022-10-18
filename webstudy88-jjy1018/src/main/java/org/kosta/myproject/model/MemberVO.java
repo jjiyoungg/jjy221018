dc8f51690855bf64374cbf460dc6d212cb70a2a0
jjiyoungg <jh99165@naver.com>
2022-10-18 16:56:25 +0900

    iu member branch에서 MemberVO id

--- webstudy88-jjy1018/src/main/java/org/kosta/myproject/model/MemberVO.java
+++ webstudy88-jjy1018/src/main/java/org/kosta/myproject/model/MemberVO.java
@@ -4,5 +4,9 @@ import java.io.Serializable;
 
 public class MemberVO implements Serializable{
 	private static final long serialVersionUID = -6079971115987091112L;
-
+	private String id;
+	public String getId() {
+		return id;
+	}
 }
+                    
